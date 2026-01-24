e9de43c3df1045e537e2289d0aa2abd9b5c832ce
Maciej <maciek333programowanie@gmail.com>
2026-01-24 15:04:35 +0100

    start comp input to required

--- src/app/docs-2-components/comp-d-input/comp-d.ts
+++ src/app/docs-2-components/comp-d-input/comp-d.ts
@@ -1,11 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, computed, forwardRef, input } from '@angular/core';
 
 @Component({
   selector: 'app-comp-d',
-  template: ``,
+  template: ` <app-comp-dd [inputName]="50" [inputReuired]="100" /> `,
+  imports: [forwardRef(() => CompDD)],
 })
 export class CompD {
   /*
+  INPUT -> mechanizm jak props w React
 
+  właściwści inputów:
+    - zmiana inputów nie możliwe w RUN_TIME 
+    - input można używać w Componentach i Dyrektywach
+    - przy dziedziczeniu komponentu jego inputy przechodzą do dziecka 
+    - nazwy inputów są case-sensitive 
+    - nowoczesne inputy są sygnałami
 	*/
+}
+
+// ###############################
+// ###############################
+// VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV
+@Component({
+  selector: 'app-comp-dd',
+  template: `
+    <div>
+      <p>Component DD</p>
+      <p>inputName = {{ inputName() }}</p>
+      <p>inputB = {{ inputB() }}</p>
+      <p>inputC = {{ inputC() }}</p>
+      <p>computedValue = {{ computedValue() }}</p>
+    </div>
+  `,
+})
+export class CompDD {
+  /*
+  określamy zmienną jako 'input' z domyślną wartością '0'
+  przypisanie wartości za pomocą nazwy 'inputName'
+  <app-comp-dd [inputName]="50" />
+  */
+  inputName = input(0);
+  inputB = input(1);
+  // inputC ma TYP <number | undefined>
+  inputC = input<number>();
+
+  computedValue = computed(() => {
+    // wartość sygnału z input jest 'read-only'
+    const inputNameVal = this.inputName();
+    return `KOMPUTED = ${inputNameVal}`;
+  });
+
+  /*
+   */
+  inputReuired = input.required<number>();
+  // inputReuired1 = input.required<number>(10);  // BŁĄD - required nie może mieć inicjalnej wartości
 }
