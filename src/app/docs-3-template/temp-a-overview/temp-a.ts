26bd0e10e76763b1161639dfb97664d1a837c8a0
Maciej <maciek333programowanie@gmail.com>
2026-02-02 19:31:36 +0100

    add template overview info

--- src/app/docs-3-template/temp-a-overview/temp-a.ts
+++ src/app/docs-3-template/temp-a-overview/temp-a.ts
@@ -4,4 +4,24 @@ import { Component } from '@angular/core';
   selector: 'app-temp-a',
   template: ` <div>TEMP_A</div> `,
 })
-export class TempA {}
+export class TempA {
+  /*
+  template -> szablon
+
+  służy do definiowania DOM (HTMLa komponentu)
+  bazuje na HTML, ale ma dodane bindowania, eventy, zmienne itp
+  kompilator Angulara czyta te szablony i zamienia
+  w bardzo wydajny kod JavaScript, który aktualizuje stronę.
+
+  ROŻNICE SZABLON vs CZYSTY HTML:
+      - komentarze w szablonie, nie trafią do HTML'a
+      - komponenty mogą byc samo zamykalne <komponent /> lub otwarte <komponent></komponent>
+      - bindowanie:
+          - [atrybutow]
+          - (eventow)
+      - znak '@' LUB '&commat;' LUB '&#64;'
+          wchodzi w tryb dynamiczny np: 'control flow'
+      - ignoruje 'whitespace'
+      - tag <script> JEST IGNOROWANY !!!
+  */
+}
